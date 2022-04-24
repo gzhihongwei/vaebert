7870b48d8389bcd03d21066971a2ed6d1bd75411
George Wei <gzhihongwei@gmail.com>
2022-04-24 17:54:00 -0400

    Fix cyclic dependency of length

--- models/vae/data.py
+++ models/vae/data.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import h5py
 import numpy as np
 
@@ -7,15 +10,17 @@ from torch.utils.data import Dataset
 class PartNetVoxelDataset(Dataset):
     def __init__(self, data_path: str) -> None:
         super().__init__()
-        
+
         self.data_path = data_path
-        
+
+        with open(os.path.join(self.data_path, "binvox_metadata.json"), "r") as f:
+            self.length = 500  # len(json.load(f)["cat_ids"])
+
     def __getitem__(self, index: int) -> np.ndarray:
         if not hasattr(self, "shapenet"):
             self.shapenet = h5py.File(self.data_path, "r")["shapes"]
-        
+
         return self.shapenet[index][np.newaxis, ...]
-        
+
     def __len__(self) -> int:
-        return len(self.shapenet)
-        
+        return self.length
