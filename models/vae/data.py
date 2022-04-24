0298ff609fd6c8f007750c7bd042cb0698d25dcf
George Wei <gzhihongwei@gmail.com>
2022-04-24 16:33:45 -0400

    Finished PartNetVoxelDataset

--- models/vae/data.py
+++ models/vae/data.py
@@ -1,17 +1,21 @@
 import h5py
 import numpy as np
 
-from pathlib import Path
-
 from torch.utils.data import Dataset
 
+
 class PartNetVoxelDataset(Dataset):
-    def __init__(self, data_path: Path):
+    def __init__(self, data_path: str) -> None:
         super().__init__()
         
         self.data_path = data_path
         
     def __getitem__(self, index: int) -> np.ndarray:
-        pass
+        if not hasattr(self, "shapenet"):
+            self.shapenet = h5py.File(self.data_path, "r")["shapes"]
         
+        return self.shapenet[index][np.newaxis, ...]
+        
+    def __len__(self) -> int:
+        return len(self.shapenet)
         
