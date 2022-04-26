c80f4764af24fc412c3b31d9cc0d8d00e7cdb683
Alexandru Munteanu <amunteanu@umass.edu>
2022-04-26 00:29:04 -0400

    Added support for train test data splits

--- models/vae/data.py
+++ models/vae/data.py
@@ -8,19 +8,29 @@ from torch.utils.data import Dataset
 
 
 class PartNetVoxelDataset(Dataset):
-    def __init__(self, data_path: str) -> None:
+    def __init__(self, data_path: str, indexes_path: str = "") -> None:
         super().__init__()
 
         self.data_path = data_path
-
+        self.indexes_path = indexes_path
+        
         with open(os.path.join(self.data_path, "binvox_metadata.json"), "r") as f:
             self.length = 500  # len(json.load(f)["cat_ids"])
 
     def __getitem__(self, index: int) -> np.ndarray:
+        
         if not hasattr(self, "shapenet"):
             self.shapenet = h5py.File(self.data_path, "r")["shapes"]
-
-        return self.shapenet[index][np.newaxis, ...]
+        
+        if indexes_path:
+            if not hasattr(self, "indexes"):
+                with open(self.indexes_path, 'r') as f:
+                    self.indexes = json.load(f)
+            used_index = self.indexes[index]
+        else:
+            used_index = index
+        
+        return self.shapenet[used_index][np.newaxis, ...]
 
     def __len__(self) -> int:
         return self.length
