089ed0422cbd05dc8b1ee9a40a06e576a51a8489
George Wei <gzhihongwei@gmail.com>
2022-05-04 10:25:16 -0400

    Simplified dataset

--- models/vae/data.py
+++ models/vae/data.py
@@ -13,16 +13,11 @@ class PartNetVoxelDataset(Dataset):
 
         self.data_path = data_path
 
-        with open(os.path.join(self.data_path, "binvox_metadata.json"), "r") as f:
-            self.length = len(json.load(f)["cat_ids"])
-
     def __getitem__(self, index: int) -> np.ndarray:
-        if not hasattr(self, "shapenet"):
-            self.shapenet = h5py.File(
-                os.path.join(self.data_path, "binvox_data.hdf5"), "r"
-            )["shapes"]
-
         return self.shapenet[index][np.newaxis, ...]
 
     def __len__(self) -> int:
-        return self.length
+        if not hasattr(self, "shapenet"):
+            self.shapenet = h5py.File(self.data_path, "r")["shapes"]
+
+        return len(self.shapenet)
