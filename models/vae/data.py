9e2a831e0a6f710fd40541bbc2d56f7af66c9214
George Wei <gzhihongwei@gmail.com>
2022-05-06 10:03:52 -0400

    Moved BERT code to separate branch

--- models/vae/data.py
+++ models/vae/data.py
@@ -1,6 +1,3 @@
-import json
-import os
-
 import h5py
 import numpy as np
 
@@ -23,24 +20,3 @@ class PartNetVoxelDataset(Dataset):
             self.shapenet = h5py.File(self.data_path, "r")["shapes"]
 
         return len(self.shapenet)
-
-
-class PartNetCaptionLatentVectorDataset(Dataset):
-    def __init__(self, data_path_captions: str, data_path_latents: str) -> None:
-        super().__init__()
-        self.data_path_captions = data_path_captions
-        self.data_path_latents = data_path_latents
-
-    def __getitem__(self, index: int) -> np.ndarray:
-        if not hasattr(self, "latents"):
-            self.latents = h5py.File(self.data_path_latents, "r")["latents"]
-            self.captions = h5py.File(self.data_path_captions, "r")["captions"]
-        return self.captions[index].decode(), self.latents[index][np.newaxis, ...]
-
-    def __len__(self) -> int:
-        if not hasattr(self, "latents"):
-            self.latents = h5py.File(self.data_path_latents, "r")["latents"]
-            self.captions = h5py.File(self.data_path_captions, "r")["captions"]
-
-        return len(self.latents)
-
